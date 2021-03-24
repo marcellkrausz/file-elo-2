ebc8fdcbe5aaaa5b60d4edac7d9bd5ce09724b0a
marcellkrausz <krauszmaarcell@gmail.com>
2021-03-24 16:10:07 +0100

    add all Class

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -4,16 +4,37 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
+    public List<String> getCompetitorsNamesFromFile(String fileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            List<Competitor> competitors = new ArrayList<>();
+            String line = "";
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] array = line.split(",");
+                competitors.add(new Competitor(
+                        array[0],
+                        new int[]{Integer.parseInt(array[1]),
+                                Integer.parseInt(array[2]),
+                                Integer.parseInt(array[3]),
+                                Integer.parseInt(array[4]),
+                                Integer.parseInt(array[5])}));
+            }
+            Collections.sort(competitors);
+            List<String> result = new ArrayList<>();
+
+            for (Competitor competitor : competitors) {
+                result.add(competitor.getName());
+            }
+
+            Collections.reverse(result);
+            return result;
+        } catch (IOException e) {
+            System.out.println("File not found!");
+        }
         return null;
     }
 
---
+++ src/main/java/com/codecool/chessopen/Competitor.java
@@ -0,0 +1,30 @@
+package com.codecool.chessopen;
+
+public class Competitor implements Comparable<Competitor> {
+    private String name;
+    private int[] results;
+    private int maxScore;
+
+    public Competitor(String name, int[] results) {
+        this.name = name;
+        this.results = results;
+        setMaxScore(results);
+    }
+
+    public void setMaxScore(int[] results) {
+        this.maxScore = results[0] + results[1] + results[2] + results[3] + results[4];
+    }
+
+    public int getMaxScore() {
+        return maxScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Competitor o) {
+        return Double.compare(this.maxScore, o.getMaxScore());
+    }
+}
